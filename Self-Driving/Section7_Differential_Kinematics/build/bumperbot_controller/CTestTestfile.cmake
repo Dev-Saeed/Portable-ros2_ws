c4e1a1437a8ae96ccdc9b962c8bca206bd477489
dev-Saeed <saeedjaber2002@gmail.com>
2024-10-09 18:04:19 +0300

    Update

---
+++ Self-Driving/Section7_Differential_Kinematics/build/bumperbot_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dev/ros2_ws/Self-Driving/Section7_Differential_Kinematics/bumperbot_ws/src/bumperbot_controller
+# Build directory: /home/dev/ros2_ws/Self-Driving/Section7_Differential_Kinematics/build/bumperbot_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Self-Driving/Section7_Differential_Kinematics/build/bumperbot_cpp_examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dev/ros2_ws/Self-Driving/Section7_Differential_Kinematics/bumperbot_ws/src/bumperbot_cpp_examples
+# Build directory: /home/dev/ros2_ws/Self-Driving/Section7_Differential_Kinematics/build/bumperbot_cpp_examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
