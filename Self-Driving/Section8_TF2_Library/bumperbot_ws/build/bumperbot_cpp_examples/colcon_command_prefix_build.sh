8551866996c63a4ed3e41d3dc283427da89dbd28
dev-Saeed <saeedjaber2002@gmail.com>
2024-10-15 11:42:09 +0300

    Update

---
+++ Self-Driving/Section8_TF2_Library/bumperbot_ws/build/bumperbot_cpp_examples/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/dev/ros2_ws/Self-Driving/Section8_TF2_Library/bumperbot_ws/install/bumperbot_msgs/share/bumperbot_msgs/package.sh"
